db1b27e5bd7f78a082b5183975065009c0a047d9
Rahmatul Usra <44527179+xsrazy@users.noreply.github.com>
2024-11-16 14:39:06 +0700

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,12 +6,12 @@ const Footer = () => {
       <div className="container mx-auto flex items-center justify-between flex-wrap">
         {/* Website Name */}
         <div className="flex items-center">
-          <h1 className="text-2xl font-bold tracking-wide">Recipefy</h1>
+          <h1 className="text-2xl font-bold tracking-wide">Masakan Kekinian</h1>
         </div>
         {/* Web Info */}
         <div className="text-sm">
-          <p>Pusaka Manggala</p>
-          <p>Bandung, Indonesia 2023</p>
+          <p>Masakan Kekinian</p>
+          <p>Jakarta, Indonesia 2023</p>
         </div>
       </div>
     </footer>
